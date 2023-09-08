6de0b11946dc3634acd5a82d5c6407af76a9f97f
Lilly Gullett <141647933+Lilly-Gullett@users.noreply.github.com>
2023-09-08 17:09:06 -0600

    Add custom grid javascript
    This commit adds the javascript needed to execute a custom grid within
    the etch-a-sketch frame.
    This does not yet have the ability to overwrite the existing grid for
    the new one.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,34 @@
-const rows = document.querySelectorAll('.row');
+const gridSlider = document.querySelector('.slider');
+const gridValueDisplay = document.querySelector('.display');
+let gridValue = gridSlider.value;
+const frame = document.querySelector('.container');
 
-for (i=1; i<17;i++) {
-    addDivs();
+gridSlider.addEventListener('click', () => {
+    gridValue = gridSlider.value;
+    gridValueDisplay.textContent=gridValue;
+    addRows(gridValue);
+    addColumns(gridValue);
+});
+
+
+function addColumns(n) {
+    for (i=0; i<n; i++) { //sets how many columns we will add
+        const rows = document.querySelectorAll('.row');
+        rows.forEach ( row => { //tells computer to add these columns to each row
+            const newSquares = document.createElement('div');
+            let size = (500/n); 
+            newSquares.setAttribute('style', 
+            `width: ${size}px;
+            height: ${size-2}px;`); //sets a dynamic size of the divs
+            row.appendChild(newSquares);
+        })
+    }
 }
 
-function addDivs() {
-    rows.forEach (row => {
-        const newSquares = document.createElement('div');
-        row.appendChild(newSquares)
-    })
+function addRows(n) {
+    for (i=0; i<n; i++) {
+        const newRows = document.createElement('div');
+        newRows.classList.add('row');
+        frame.appendChild(newRows);
+    }
 }
